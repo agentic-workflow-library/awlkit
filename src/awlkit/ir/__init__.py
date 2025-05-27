44fd474c6cf65fee6cb356867a20f760a3726206
Dave Roberson <davidroberson@users.noreply.github.com>
2025-05-27 09:03:45 +0000

    more work

--- src/awlkit/ir/__init__.py
+++ src/awlkit/ir/__init__.py
@@ -1,17 +1,19 @@
 """Intermediate Representation for workflow definitions."""
 
-from .workflow import Workflow
+from .workflow import Workflow, WorkflowCall
 from .task import Task
 from .io import Input, Output
 from .runtime import Runtime
-from .types import WorkflowType, DataType
+from .types import WorkflowType, DataType, TypeSpec
 
 __all__ = [
     "Workflow",
+    "WorkflowCall",
     "Task",
     "Input", 
     "Output",
     "Runtime",
     "WorkflowType",
     "DataType",
+    "TypeSpec",
 ]
